607fae93cec4a9f445f2a61a20830a0096812bf7
Abdisatar Yusuf <satexza12@gmail.com>
2025-11-07 19:07:12 +0300

    first commit

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,4 @@
-import WelcomeMessage from './components/WelcomeMessage';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <WelcomeMessage />
-      {/* Other existing components */}
-    </div>
-  );
-}
-
-export default App;
-
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
