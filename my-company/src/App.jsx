a1f4298c663e1ffd79da750f9bf015ce1472d3a2
Abdisatar Yusuf <satexza12@gmail.com>
2025-11-13 14:34:15 +0300

    Update App.jsx

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -102,4 +102,23 @@ function App() {
         </Router>
     );
 }
-export default App
+
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Recipe Sharing App</h1>
+      </header>
+      <main className="app-main">
+        <AddRecipeForm />
+        <RecipeList />
+      </main>
+    </div>
+  );
+}
+
+export default App;
