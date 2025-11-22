53cef5207689846cd8eceb17737987030e3b3c79
Abdisatar Yusuf <satexza12@gmail.com>
2025-11-22 10:35:00 +0300

    Update vite.config.js

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -32,3 +32,13 @@ export default defineConfig({
     port: 3000,
   }
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
