1489cfe4f655273e23698bd49090c5f5f16fe3f7
Abdisatar Yusuf <satexza12@gmail.com>
2025-11-22 10:29:02 +0300

    Update vite.config.js

--- github-user-search/vite.config.js
+++ github-user-search/vite.config.js
@@ -5,3 +5,30 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          utils: ['axios']
+        }
+      }
+    }
+  },
+  server: {
+    port: 3000,
+  },
+  preview: {
+    port: 3000,
+  }
+})
