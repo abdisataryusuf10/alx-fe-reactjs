196727d4d91768124766da0bea222ffef70b7903
Abdisatar Yusuf <satexza12@gmail.com>
2025-11-26 20:47:36 +0300

    Update App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -32,4 +32,15 @@ function App() {
   )
 }
 
-export default App
+import React from 'react';
+import UserProfile from './components/UserProfile';
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfile />
+    </div>
+  );
+}
+
+export default App;
